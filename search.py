e07c7f70bed05baaac1023041527d406ab64bc60
Evan Kuvshinoff <evan@kuvsinoff.net>
2025-01-01 11:17:58 -0500

    formatted

--- search.py
+++ search.py
@@ -1,4 +1,3 @@
-
 import json
 import os
 import requests
@@ -11,42 +10,39 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-
-
-endpoint =  "https://api.bing.microsoft.com/v7.0/search"
+endpoint = "https://api.bing.microsoft.com/v7.0/search"
 subscription_key = os.environ.get("SUBSCRIPTION_KEY")
-#get your own azure key freeloader 
+# get your own azure key freeloader
 
 
 def search(query):
 
-    print("searching web for " + query + "\n\n" )
+    print("searching web for " + query + "\n\n")
 
-    mkt = 'en-US'
-    params = { 'q': query, 'mkt': mkt, 'responseFilter': {"Webpages","News"},  'count': "1" }
-    headers = { 'Ocp-Apim-Subscription-Key': subscription_key }
-
-
+    mkt = "en-US"
+    params = {
+        "q": query,
+        "mkt": mkt,
+        "responseFilter": {"Webpages", "News"},
+        "count": "1",
+    }
+    headers = {"Ocp-Apim-Subscription-Key": subscription_key}
 
     response = requests.get(endpoint, headers=headers, params=params)
     response.raise_for_status()
     search_results = response.json()
-   
-    pages = search_results['webPages']
-    results = pages['value']
+
+    pages = search_results["webPages"]
+    results = pages["value"]
     output = ""
 
-    
-    
-    
     for result in results:
-        content = requests.get(result['url']).content
+        content = requests.get(result["url"]).content
 
-        #when in python, just throw imports at the problem and learn nothing yourself 
-        soup = BeautifulSoup(content, 'html.parser')
-        text = soup.find('body').get_text().strip()
+        # when in python, just throw imports at the problem and learn nothing yourself
+        soup = BeautifulSoup(content, "html.parser")
+        text = soup.find("body").get_text().strip()
 
-        cleantext = ' '.join(text.split('\n'))
-        cleantext = ' '.join(text.split())
+        cleantext = " ".join(text.split("\n"))
+        cleantext = " ".join(text.split())
         return cleantext
-
